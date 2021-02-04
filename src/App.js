645fd16c1cd69cb1d157d02cc4d1f335fd19200d
sujancse5bu <60531496+sujancse5bu@users.noreply.github.com>
2021-02-04 11:20:09 +0600

    ProductListScreen added

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ import PlaceOrderScreen from './screens/PlaceOrderScreen'
 import OrderScreen from './screens/OrderScreen'
 import UserListScreen from './screens/UserListScreen'
 import UserEditScreen from './screens/UserEditScreen'
+import ProductListScreen from './screens/ProductListScreen'
 
 function App() {
   return (
@@ -33,6 +34,7 @@ function App() {
           <Route path='/cart/:id?' component={CartScreen} />
           <Route path='/admin/user-list' component={UserListScreen} />
           <Route path='/admin/user/:id/edit' component={UserEditScreen} />
+          <Route path='/admin/product-list' component={ProductListScreen} />
           <Route path='/' component={HomeScreens} exact />
         </Container>
       </main>
